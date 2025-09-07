8cb0e2d2627366e69b76ae1a7e2f2fbbfe678496
Tarajyothi26 <24wh1a05v3@bvrithyderabad.edu.in>
2025-09-07 22:37:01 +0530

    Update HierarchyInheritance.java

--- HierarchyInheritance.java
+++ HierarchyInheritance.java
@@ -1,30 +1,46 @@
-class Vehicle {
-    void wheels() {
-        System.out.println("All vehicles have wheels");
+HIERARCHICAL INHERITANCE
+Hierarchical inheritance is a type of inheritance in Java where a single parent class 
+is inherited by multiple child classes. In this structure, also known as one-to-many
+inheritance, a common parent or base class serves as the foundation for several specialized 
+subclasses, creating a tree-like hierarchy. Each child class inherits the common properties 
+and methods of the parent, promoting code reusability and providing a consistent base for all
+the subclasses. However, each child class can also define its own unique attributes and methods, 
+allowing it to specialize and differentiate itself from its siblings. This structure is useful for
+organizing related but distinct classes that share a core set of functionality, such as a Vehicle
+class with child classes like Car, Bike, and Truck. 
+
+CODE:
+class Animal {
+    void sound() {
+        System.out.println("Animals make sounds");
     }
 }
 
-class Car extends Vehicle {
-    void fuelType() {
-        System.out.println("Car runs on petrol or diesel");
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
     }
 }
 
-class Bike extends Vehicle {
-    void fuelType() {
-        System.out.println("Bike runs on petrol");
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
     }
 }
 
-public class HierarchyInheritance {
+public class HierarchicalInheritance {
     public static void main(String[] args) {
-        Car c = new Car();
-        Bike b = new Bike();
+        Dog d = new Dog();
+        d.sound();
+        d.bark();
 
-        c.wheels();
-        c.fuelType();
-
-        b.wheels();
-        b.fuelType();
+        Cat c = new Cat();
+        c.sound();
+        c.meow();
     }
 }
+OUTPUT:
+Animals make sounds
+Dog barks
+Animals make sounds
+Cat meows
